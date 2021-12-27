af699f13518de2275ac5c27ce9b7b8b699e6a92a
Fylls <55017307+Fylls@users.noreply.github.com>
2021-12-27 01:49:54 +0100

    experiment1 code

---
+++ analysis/statistics1.R
@@ -0,0 +1,47 @@
+# magnetic permittivity of free space
+mu = 4 * pi * 10^-7
+
+# Data
+radius1 = 0.105
+radius2 = 0.015
+windings1 = 200
+windings2 = 2000
+frequencies = c(10,20,30,40)
+voltage1 = c(2.874,2.780,2.664,2.523) * 0.0506 # non ho capito se voltaggio o
+voltage2 = c(0.024,0.037,0.041,0.049)
+
+# mutual inductance
+M = (mu * windings1 * windings2 * radius2^2) / (2 * radius1)
+
+# Linear regression
+model = lm( voltage2 ~ I( voltage1 * frequencies  * 2 * pi ) )
+summary(model)
+
+# Plots with regression line
+plot(voltage1 * frequencies  * 2 * pi, voltage2)
+abline(model)
+
+# Residuals
+res = resid(model)
+
+# Residual VS Fitted plot
+plot(fitted(model), res)
+abline(0,0)
+
+# Shapiro-Wilk
+shapiro.test(res)
+shapiro.test(voltage1)
+shapiro.test(voltage2)
+
+# QQ-plot 
+qqnorm(res)
+qqline(res)
+qqnorm(voltage1)
+qqline(voltage1)
+qqnorm(voltage2)
+qqline(voltage2)
+
+# Density
+plot(density(res))
+plot(density(voltage1))
+plot(density(voltage2))
