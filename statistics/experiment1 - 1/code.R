a2da3add6739017c3e822676d7834aa3f9af5ac8
Fylls <55017307+Fylls@users.noreply.github.com>
2021-12-27 02:08:37 +0100

    experiment1 statistics

--- statistics/experiment1 - 1/code.R
+++ statistics/experiment1 - 1/code.R
@@ -1,4 +1,4 @@
-# magnetic permittivity of free space
+# Magnetic permittivity of free space
 mu = 4 * pi * 10^-7
 
 # Data
@@ -6,19 +6,19 @@ radius1 = 0.105
 radius2 = 0.015
 windings1 = 200
 windings2 = 2000
-frequencies = c(10,20,30,40)
-voltage1 = c(5.748,5.561,5.280,4.953) * 0.0506 # non ho capito se voltaggio o
-voltage2 = c(0.034,0.062,0.081,0.112)
+frequency = c(10,20,30,40)
+current1 = c(2.874,2.780,2.664,2.523) * 0.0506 # non ho capito se voltaggio o
+voltage2 = c(0.024,0.037,0.041,0.049)
 
 # mutual inductance
 M = (mu * windings1 * windings2 * radius2^2) / (2 * radius1)
 
 # Linear regression
-model = lm( voltage2 ~ I( voltage1 * frequencies  * 2 * pi ) )
+model = lm( voltage2 ~ I( current1 * frequency  * 2 * pi ) )
 summary(model)
 
 # Plots with regression line
-plot(voltage1 * frequencies  * 2 * pi, voltage2)
+plot(current1 * frequency  * 2 * pi, voltage2)
 abline(model)
 
 # Residuals
@@ -30,18 +30,18 @@ abline(0,0)
 
 # Shapiro-Wilk
 shapiro.test(res)
-shapiro.test(voltage1)
+shapiro.test(current1)
 shapiro.test(voltage2)
 
 # QQ-plot 
 qqnorm(res)
 qqline(res)
-qqnorm(voltage1)
-qqline(voltage1)
+qqnorm(current1)
+qqline(current1)
 qqnorm(voltage2)
 qqline(voltage2)
 
 # Density
 plot(density(res))
-plot(density(voltage1))
+plot(density(current1))
 plot(density(voltage2))
